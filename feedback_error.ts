a0e7b3e64a2bf1d9840e1a9b7f607d86c476c4e0
Pavel Plekhanov <75085214+Pauolosavery@users.noreply.github.com>
2025-07-24 12:13:41 +0300

    Create feedback_error.ts

---
+++ feedback_error.ts
@@ -0,0 +1,12 @@
+(0 , markdown_draft_js__WEBPACK_IMPORTED_MODULE_1__.stateToMarkdown) is not a function
+TypeError: (0 , markdown_draft_js__WEBPACK_IMPORTED_MODULE_1__.stateToMarkdown) is not a function
+    at Object.handleEditorChange [as onChange] (webpack-internal:///./src/pages/administration/notification-distribution/components/EditableTextArea.tsx:34:96)
+    at DraftEditor.eval [as update] (webpack-internal:///./node_modules/draft-js/lib/DraftEditor.react.js:371:19)
+    at editOnFocus (webpack-internal:///./node_modules/draft-js/lib/editOnFocus.js:40:12)
+    at eval (webpack-internal:///./node_modules/draft-js/lib/DraftEditor.react.js:459:13)
+    at HTMLUnknownElement.callCallback (webpack-internal:///./node_modules/react-dom/cjs/react-dom.development.js:3945:14)
+    at Object.invokeGuardedCallbackDev (webpack-internal:///./node_modules/react-dom/cjs/react-dom.development.js:3994:16)
+    at invokeGuardedCallback (webpack-internal:///./node_modules/react-dom/cjs/react-dom.development.js:4056:31)
+    at invokeGuardedCallbackAndCatchFirstError (webpack-internal:///./node_modules/react-dom/cjs/react-dom.development.js:4070:25)
+    at executeDispatch (webpack-internal:///./node_modules/react-dom/cjs/react-dom.development.js:8243:3)
+    at processDispatchQueueItemsInOrder (webpack-internal:///./node_modules/react-dom/cjs/react-dom.development.js:8275:7)
